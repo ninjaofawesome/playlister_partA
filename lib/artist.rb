52ea557aa9ca85a856efbc43b4e72e05201a88a5
hannah nordgren <hannah.nordgren@flatironschool.com>
2013-11-26 13:05:21 -0500

    regex in progress

---
+++ lib/artist.rb
@@ -0,0 +1,9 @@
+Dir["./spec/*.rb"].each {|file| require file }
+Dir["./data/*.mp3"].each {|file| require file }
+
+
+
+
+class Artist
+
+end
---
+++ lib/song.rb
@@ -0,0 +1,39 @@
+Dir["./spec/*.rb"].each {|file| require file }
+Dir["./data/*.mp3"].each {|file| require file }
+
+
+class Song
+
+	attr_reader :song_title, :genre, :artist
+
+	def initialize
+		@song_title = []
+		@genre = []
+		@artist = []
+	end
+
+	#get more general!
+	# def initialize(song_title, genre, artist)
+	# 	@song_title = song_title
+	# 	@genre = genre
+	# 	@artist = artist
+	# end
+
+
+	def input_song_title
+		song_title << "Peso"
+		puts song_title
+	end
+	
+	# def input_song_title
+	# 	song_title << "Peso"
+	# 	puts song_title
+	# end
+
+
+
+
+end
+baller_song = Song.new
+# baller_song = Song.new("Peso", "dance", "A$AP Rocky")
+puts baller_song.inspect
